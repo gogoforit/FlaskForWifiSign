347d3e9d52f9afc575c6d8ae4c290ed0d7837aa4
gogoforit <715157026@qq.com>
2017-05-04 16:17:57 +0800

    完成可以查询某一天签到情况

--- FlaskForWifiSign.py
+++ FlaskForWifiSign.py
@@ -1,12 +1,33 @@
 from flask import Flask,flash,render_template,request
+from MongodbConn import MongoPipeline
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
+@app.route('/query',methods={'POST'})
+def query():
+    form = request.form
+    name = form.get('name')
+    date = form.get('date')
+    conn = MongoPipeline()
+    conn.open_connection('qiandao_last_info')
+    ids = conn.getIds('info', {'name': name, 'date': date})
+    _id = next(ids, None)
+    sign_info = []
+    while _id:
+        # print(_id)
+        sign_info.append(_id)
+        _id = next(ids, None)
+    print(sign_info)
+    return render_template('query.html',sign_info = sign_info)
+
+@app.route('/query_info')
+def query_info():
+    return render_template('query.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
